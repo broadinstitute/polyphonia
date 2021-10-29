c2d84bf7b614cfe51e15db6a8e308a92c5500dc6
Lydia Andreyevna Krasilnikova <lakras@mit.edu>
2021-10-29 11:07:33 -0400

    set to 1 core

--- bin/detect_potential_cross_contamination.pl
+++ bin/detect_potential_cross_contamination.pl
@@ -327,6 +327,9 @@ for($argument_index = 0; $argument_index <= $#ARGV; $argument_index++)
 	}
 }
 
+# added for testing--DELETE LATER
+$cores_to_use = 1;
+
 
 # verifies that all necessary command line arguments have been included
 if(!$reference_genome_file)
