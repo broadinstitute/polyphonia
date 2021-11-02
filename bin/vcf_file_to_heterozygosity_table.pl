b9eb1616901d07dbfefea69bf19a46c3c394b331
Lydia Andreyevna Krasilnikova <lakras@mit.edu>
2021-11-02 16:10:50 -0400

    phrasing in comment

--- bin/vcf_file_to_heterozygosity_table.pl
+++ bin/vcf_file_to_heterozygosity_table.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/env perl
 
-# Reads in vcf file output produced by LoFreq call and prints human-readable heterozygosity
-# table. Optionally filters output by read depth and minor allele readcount and frequency.
+# Reads in vcf file output produced by LoFreq call and prints human-readable
+# heterozygosity table. Optionally filters output by read depth, minor allele readcount,
+# and minor allele frequency.
 
 # See https://csb5.github.io/lofreq/commands/#call for information on LoFreq call.
 
